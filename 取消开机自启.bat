bff64547e5c080cbf9275c7ede8d2e9d6cabd77a
dffgdfg1 <3252511552@qq.com>
2026-08-06 11:31:22 +0800

    取消脚本改用PowerShell停进程(本机无wmic)

--- 取消开机自启.bat
+++ 取消开机自启.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v TOCUpdateService /f >nul 2>nul
-wmic process where "name='pythonw.exe' and commandline like '%%toc_service%%'" delete >nul 2>nul
+powershell -NoProfile -Command "Get-CimInstance Win32_Process -Filter \"name='pythonw.exe'\" | Where-Object { $_.CommandLine -like '*toc_service*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }" >nul 2>nul
 echo.
 echo [OK] Auto-start removed, and the running service was stopped.
 echo.
